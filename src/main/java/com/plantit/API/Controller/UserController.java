d13d6b056bb48b8688618a10bafa1bd1f281e8e5
Maxime38390 <maxime.guerin@epsi.fr>
2023-03-03 00:35:41 +0100

    [test] test merge branches

--- src/main/java/com/plantit/API/Controller/UserController.java
+++ src/main/java/com/plantit/API/Controller/UserController.java
@@ -1,19 +1,16 @@
 package com.plantit.API.Controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.plantit.BLL.ManageUser;
 import com.plantit.DATA.dal.entities.User;
-import com.plantit.DATA.dal.repositories.UserRepository;
 import com.plantit.DATA.dto.UserDTO;
-import com.plantit.DATA.dto.service.UserService;
-import com.plantit.DATA.dto.service.impl.UserServiceImpl;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,57 +18,79 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/customers")
-@Api(value = "UserController", tags = { "User Management" })
+@Api(value = "UserController", tags = {"User Management"})
 public class UserController {
 
     private final ManageUser manageUser;
-    private final UserRepository userRepository;
+    private final ObjectMapper objectMapper;
 
     @Autowired
-    public UserController(ManageUser manageUser,
-                          UserRepository userRepository) {
+    public UserController(ManageUser manageUser, ObjectMapper objectMapper) {
         this.manageUser = manageUser;
-        this.userRepository = userRepository;
+        this.objectMapper = objectMapper;
     }
 
-    @PostMapping("")
+    @PostMapping("/createCustomer")
     @ApiOperation(value = "Create a new customer", response = void.class)
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Customer created successfully"),
             @ApiResponse(code = 400, message = "Invalid input data")
     })
-    public void createCustomer(@RequestBody UserDTO userDTO) {
-        manageUser.createCustomer(userDTO);
+    public ResponseEntity<User> createCustomer(@RequestBody UserDTO userDTO) {
+        User user = manageUser.createCustomer(userDTO);
+        return ResponseEntity.ok().body(user);
     }
 
-    // handler method to handle list of users
-    @GetMapping("")
-//    public String users(Model model){
-//        List<UserDTO> users = manageUser.findAllUsers();
-//        model.addAttribute("users", users);
-//        return "users";
-//    }
+    @PostMapping("/createBotanist")
+    @ApiOperation(value = "Create a new customer", response = void.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Customer created successfully"),
+            @ApiResponse(code = 400, message = "Invalid input data")
+    })
+    public ResponseEntity<User> createBotanist(@RequestBody UserDTO userDTO) {
+        User user = manageUser.createBotanist(userDTO);
+        return ResponseEntity.ok().body(user);
+    }
 
+    @PostMapping("/login")
+    @ApiOperation(value = "Login", response = void.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Login successfull"),
+            @ApiResponse(code = 400, message = "Login failed")
+    })
+    public ResponseEntity<UserDTO> Login(String login, String password) {
 
+        Optional<UserDTO> userDTO = Optional.ofNullable(manageUser.Login(login, password));
+        return userDTO.map(dto -> new ResponseEntity<>(dto, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
 
-    // handler method to handle user registration form submit request
-    @PostMapping("/register/save")
-    public String registration(@Valid @ModelAttribute("user") UserDTO userDto,
-                               BindingResult result,
-                               Model model){
-        User existingUser = userRepository.findByEmailIgnoreCase(userDto.getEmail());
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDTO> getUserById(@PathVariable Long userId) {
+        Optional<UserDTO> userDTO = Optional.ofNullable(manageUser.getUserById(userId));
+        return userDTO.map(dto -> new ResponseEntity<>(dto, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
 
-        // On vérifie qu'il n'y a pas déjà l'email présent dans la base
-        if(existingUser != null && existingUser.getEmail() != null && !existingUser.getEmail().isEmpty()){
-            result.rejectValue("email", null,
-                    "Il y a déjà un compte possédant la même adresse mail");
-        }
+    @GetMapping("/users")
+    public List<UserDTO> listAllUsers() {
+        return manageUser.getAllUsers();
+    }
 
-        if(result.hasErrors()){
-            model.addAttribute("user", userDto);
-            return "/register";
-        }
-        manageUser.createUser(userDto);
-        return "redirect:/register?success";
+    @PutMapping("/{userId}")
+    public ResponseEntity<UserDTO> updateUser(@PathVariable Long userId, @RequestBody UserDTO userDTO) {
+        User user = manageUser.updateUser(userId, userDTO);
+        UserDTO updatedUserDTO = objectMapper.convertValue(user, UserDTO.class);
+        return ResponseEntity.ok().body(updatedUserDTO);
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long userId) {
+        manageUser.deleteUser(userId);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/type/{userTypeId}")
+    public ResponseEntity<List<UserDTO>> getUsersByType(@PathVariable Long userTypeId) {
+        List<UserDTO> users = manageUser.getUsersByType(userTypeId);
+        return new ResponseEntity<>(users, HttpStatus.OK);
     }
 }
