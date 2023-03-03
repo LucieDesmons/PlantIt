e00d1ea32e6a0da9b386a5447ac97e2d055dffe6
baptiste <bap.martin99@hotmail.fr>
2023-03-03 12:49:52 +0100

    améliorations5

--- src/main/java/com/plantit/controllers/PlantControllerFront.java
+++ src/main/java/com/plantit/controllers/PlantControllerFront.java
@@ -12,26 +12,29 @@ import com.plantit.API.Controller.PlantReferenceController;
 import com.plantit.DATA.dto.PlantDTO;
 import com.plantit.DATA.dto.PlantReferenceDTO;
 import com.plantit.DATA.dto.UserDTO;
+import ch.qos.logback.core.model.Model;
 
 @Controller
 public class PlantControllerFront{
     private PlantController back;
     private PlantReferenceController backRef;
 
-    @GetMapping(value = "/show_plant")
-    public PlantDTO new_plant(PlantDTO plante) {
+    @PostMapping(value = "/show_plant")
+    public PlantDTO new_plant(Model model) {
         UserDTO user = new UserDTO();   //récupère l'ID de l'utilisateur connecté
+        //model.addAttribute("plant", new PlantDTO());
+        PlantDTO plante = new PlantDTO();
         plante.setUserDTO(user);
         return plante;
     }
 
-    @PostMapping(value = "/get_plant")
+    @GetMapping(value = "/get_plant")
     public List<PlantDTO> fiche_plante(@ModelAttribute(value="plante") long id) {
         List<PlantDTO> liste = back.getUserPLants(id).getBody();
         return liste;
     }
 
-    @PostMapping(value = "/send_all_plant_reference")
+    @GetMapping(value = "/send_all_plant_reference")
     public List<String> liste_especes() {
         List<PlantReferenceDTO> listeRefs = backRef.listAllPlants().getBody();
         List<String> liste = new ArrayList();
@@ -42,7 +45,7 @@ public class PlantControllerFront{
         return liste;
     }
 
-    @GetMapping(value = "/delete_plant")
+    @PostMapping(value = "/delete_plant")
     public Long delete_plant(PlantDTO plante){
         return plante.getIdPlantDTO();
     }
